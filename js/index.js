70ae506a342a76c874ffe0a194e11932ec9144da
Adams Ahmed <ifeoluwadolapo5@gmail.com>
2022-07-11 16:50:02 +0100

    in a bit

--- js/index.js
+++ js/index.js
@@ -1,79 +1,150 @@
 
-let input1, input2, sumUP, minus, divide, times, operan, tall;
+let input1, input2, sumUP, divide, times, operan, tall;
 let press = false;
-let screen = document.getElementById('screen').innerHTML
-function calculator() {
-    document.querySelector(".body").style.display = "inherit";
-    document.querySelector('#for').style.display = "none"
+let click = false;
+let screen = document.getElementById('screen')
+
+
+
+
+let switchPower = document.querySelector('.power').innerHTML
+function power() {
+    let switchPower = document.querySelector('.power').innerHTML
+    if (switchPower == 'ON') {
+        document.querySelector('.power').style.backgroundColor = "blue"
+        document.querySelector('.power').innerHTML = "OFF"
+        document.querySelector('.screen').style.backgroundColor = "rgb(240, 236, 236)"
+        click = true;
+    } else {
+        document.querySelector('.power').innerHTML = "ON"
+        document.querySelector('.power').style.backgroundColor = "red"
+        document.querySelector('.screen').style.backgroundColor = "grey"
+        document.querySelector('.screen').innerHTML = ""
+        document.querySelector('.top').innerHTML = ""
+        click = false;
+    }
 }
-function onScreen() {
-
-
-    document.querySelector("#screen").style.display = "inherit"
-    document.querySelector("#top").style.display = "inherit"
-    document.querySelector(".on").style.display = "none"
-    document.querySelector(".off-screen").style.display = "none"
-    document.querySelector(".off").style.display = "inherit"
-    document.querySelector('.screen').innerHTML = "0"
-    document.querySelector('.top').innerHTML = ""
-    screen = document.querySelector('.welcome').style.display = "inherit"
-}
-function offScreen() {
-    document.querySelector('.off-screen').style.display = "inherit"
-    document.querySelector('.screen').style.display = "none"
-    document.querySelector('.top').style.display = "none"
-    document.querySelector('.on').style.display = "inherit"
-    document.querySelector('.off').style.display = "none"
-  
+function clickOut() {
+    document.getElementById('top').innerHTML = ""
+   screen.innerHTML = ""
 
 }
 
-lott = screen.innerHTML
-function clickMe(click) {
-
+function clickMe(numbers) {
     let screen = document.getElementById('screen')
 
+    if (!click) {
+        return;
+    }
     if (operan != "") {
         screen.innerHTML = ""
-        press = false
         operan = ""
 
     }
-    screen.innerHTML += click
+    // if (press) {
+    //     press = false
+    //     screen.innerHTML = ""
+    // }
+    screen.innerHTML += numbers
     input2 = screen.innerHTML
-
-
 }
 
-function opera(sign) {
+function cScreen() {
+    if (!click) {
+        return;
+    }
+    screen.innerHTML = ""
 
-    if (input1) {
-        operaa()
+}
+function squareRoot() {
+    if (!click) {
+        return;
+    }
+    document.getElementById('top').innerHTML = `&radic; (${screen.innerHTML})`
+    let root = Math.sqrt(screen.innerHTML)
+    document.getElementById('screen').innerHTML = root
+}
+function square() {
+    if (!click) {
+        return;
+    }
+    document.getElementById('top').innerHTML = `sqr (${screen.innerHTML})`
+    let squarr = Math.pow(screen.innerHTML, 2);
+    document.getElementById('screen').innerHTML = squarr;
+}
+function negative(sette) {
+
+    if (sette == '-') {
+        screen.innerHTML = "-" + screen.innerHTML
+        let sette = ""
+        console.log(sette);
+    }
+    else {
+        screen.innerHTML = screen.innerHTML
+      let  sette = '-'
+        console.log(sette);
+    }
+}
+function over() {
+    if (!click) {
+        return;
+    }
+    document.getElementById('top').innerHTML = "1/" + (screen.innerHTML);
+    let overOne = 1 / screen.innerHTML
+    document.getElementById('screen').innerHTML = overOne
+}
+function del() {
+
+    let count = screen.innerHTML
+    let spac = count.slice(0, count.length - 1)
+    screen.innerHTML = spac
+}
+function operators(signs) {
+    if (!click) {
+        return;
     }
     let top = document.getElementById('top')
     screen2 = document.getElementById('screen').innerHTML
-
-
-    top.innerHTML = screen2 + sign
+    // let input = input1 || input2
+    if (input1) {
+        operaa()
+    }
+    top.innerHTML = screen2 + signs
     lot = screen2
     input1 = lot
-    operan = sign
-    sumUP = sign
-    // console.log(top.innerHTML);
-    // press = true
-    divide = top.innerHTML
+    operan = signs
+    sumUP = signs
+    press = true
+    // divide = top.innerHTML
+}
+function modulus() {
+    let top = document.getElementById('top')
+    // if (input1 =="") {
+    //     screen.innerHTML = 0
+    //     top.innerHTML = "" 
+    //     return;
+    // }
+
+
+    // console.log(sumUP);
+    if (input1 != "") {
+        let tott = Math.pow(input1, 2) / 100
+        screen.innerHTML = tott
+        top.innerHTML = input1 + sumUP + screen.innerHTML
+        
+    }
+
+
 }
 let ans
 function operaa(lol) {
-
+    if (!click) {
+        return;
+    }
     let top2 = document.getElementById("top")
-
     if (lol == '=' && ans) {
-        // console.log(ans);
         let tall = ans
         input1 = tall
-        top2.innerHTML = input1 += sumUP + input2 + lol
-
     }
 
     if (sumUP === '*') {
@@ -89,29 +160,18 @@ function operaa(lol) {
         top2.innerHTML = input1 += sumUP + input2 + lol
     }
     else if (sumUP === "/") {
-        ans = Number(input1) ^ Number(input2)
+        ans = Number(input1) / Number(input2)
         top2.innerHTML = input1 += sumUP + input2 + lol
-    }else {
-        ans = "0"
-        top2.innerHTML = ""
     }
-    
-
     document.getElementById('screen').innerHTML = ans
-
-
-
 }
-function clickOut() {
 
-    document.getElementById('screen').innerHTML = ''
-    document.getElementById('top').innerHTML = ""
-}
 let body = document.querySelector('.body')
 function cleaqr() {
     body.style.display = 'none'
     document.querySelector('#for').style.display = "inherit"
 }
+
 // let a = 5
 // let b = 5
 //  b +=a
